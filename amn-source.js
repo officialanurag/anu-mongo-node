c0ed302b4953460d8bb3ae355f8eb74789e3e675
Anurag Gupta <>
2018-11-19 16:27:24 +0530

    new changes

--- amn-source.js
+++ amn-source.js
@@ -1,7 +1,7 @@
 /**
  * Created On October 24, 2018
  *
- * Description: This is source file for loading library. It contains driver, connection
+ * Description: Contains mongoDB CRUD functions. It contains driver, connection
  * and method to drive operation on mongoDB. 
  *
  * MIT License
@@ -20,7 +20,8 @@ var DATABASE = 'myDB'; // Enter Database name here.
  */
 
 try{
-	var mongo = require('mongodb').MongoClient; 
+	var mongo = require('mongodb').MongoClient; 	
+	var ObjectID = require('mongodb').ObjectID;
 	emit('MongoDB client initiated successfully.');	
 }
 catch(err){
@@ -34,38 +35,124 @@ catch(err){
  * 
  */
 
-function anu_mongo_exec(action,payload,condition){	
+function anu_mongo_exec(action,dataObject,condition, callback){	
 	mongo.connect(URL, { useNewUrlParser: true }, function(err, db){
 		var dbo = db.db(DATABASE);
 		switch(action){
-			case 'insertOne':				
+			case 'insertOne':						        
+				dbo.collection(dataObject.collection).insertOne((dataObject.payload)[0], function(err, res) {
+				    if (err) throw err;
+				    emit('Query Result: ' + res.result);
+				    emit('Inserted Documents: ' + res.insertedCount);
+				    callback(res.ops);
+				    db.close();
+				});			
 			break;
 
 			case 'insert':
+				dbo.collection(dataObject.collection).insertMany(dataObject.payload, function(err, res) {
+				    if (err) throw err;
+				    emit('Query Result: ' + JSON.stringify(res.result));
+				    emit('Inserted Documents: ' + res.insertedCount);
+				    callback(res.ops);
+				    db.close();
+				});
 			break;
+
+			case 'delete':
+				dbo.collection(dataObject.collection).deleteMany(dataObject.payload, function(err, res) {
+				    if (err) throw err;
+				    emit(res.result.n + " document(s) deleted");
+				    db.close();
+				});
+			break;
+
+			case 'update':
+				dbo.collection(dataObject.collection).updateOne(condition, dataObject.payload, function(err, res) {
+				    if (err) throw err;
+				    emit(console.log(res.result.nModified + " document(s) updated"));
+				    db.close();
+				});
+			break;			
 		}
 	});			
 }
 
+module.exports = {
+	/**
+	 * Description: To insert data in mongoDB.
+	 * @method insert
+	 * @param collection, payload, callback function
+	 *
+	 * @return console output
+	 */
 
-/**
- * Description: To insert data in mongoDB.
- * @method insert
- * @param collection, payload
- *
- * @return console output
- */
+	insert : function (collection, payload, callback){
+		var count = Object.keys(payload).length;		
+		
+		var PAYLOAD = {
+			'collection' : collection,
+			'payload' : payload
+		};		
+		
+		if(count == 1){
+			anu_mongo_exec('insertOne', PAYLOAD, null, function(output) {
+				callback(output);
+			});
+		}
+		else{
+			anu_mongo_exec('insert', PAYLOAD, null, function(output) {
+				callback(output);
+			});
+		}		
+	},
 
-function insert(collection, payload){
-	var count = Object.keys(payload).length;
-	if(count == 1){
-		anu_mongo_exec('insertOne', payload, null);
+	/**
+	 * Description: To update data in mongoDB.
+	 * @method update
+	 * @param collection, payload, callback function
+	 *
+	 * @return console output
+	 */
+
+	update : function (collection, condition, payload, callback){
+		var PAYLOAD = {
+			'collection' : collection,			
+			'payload' : payload
+		};	
+		anu_mongo_exec('update', PAYLOAD, condition, function(output) {
+			callback(output);
+		});
+	}, 
+
+	/**
+	 * Description: To delete data in mongoDB.
+	 * @method delete
+	 * @param collection, payload, callback function
+	 *
+	 * @return console output
+	 */	
+
+	delete : function (collection, payload, callback){
+		var PAYLOAD = {
+			'collection' : collection,
+			'payload' : payload
+		};	
+		anu_mongo_exec('delete', PAYLOAD, null, function(output) {
+			callback(output);
+		});
+	},
+
+	/**
+	 * ObjectID for _id field
+	 */
+
+	oid : function(value) {
+		return ObjectID(value);
 	}
-	else{
-		anu_mongo_exec('insert', payload, null);	
-	}
-	
 }
+
+
 
 
 /**
@@ -79,12 +166,3 @@ function insert(collection, payload){
 function emit(msg){
 	console.log(msg);
 }
-
-insert('chatRooms', 
-	[
-		{
-			name : 'lele',
-			roomName : 'roomelee'
-		}
-	]
-);
